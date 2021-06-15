729ac4a2ff31a524514c7120a9cab36d17b7e8c6
winterguard <aliasbonfire@gmail.com>
2021-06-15 15:00:13 +0900

    add other component

---
+++ components/Result/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+   RESULT : {{ this.computedTodoTitle }}
+  </div>
+</template>
+
+<script>
+import { TodoItem } from '@/classes';
+
+export default {
+    props: {
+        todo: {
+            type: TodoItem,
+            require: true,
+            default: () => ({
+                title: 'EMPTY',
+            })
+        }
+    },
+    data() {
+        return {
+            todoTitle: ''
+        }
+    },
+    computed: {
+        computedTodoTitle() {
+            console.log('changed Todo Title');
+            return this.todo.title;
+        }
+    },
+    watch: {
+        todo: {
+            deep: true,
+            handler() {
+                console.log('Property Changed', this.todo);
+            }
+        }
+    },
+    methods: {
+        changeTitle() {
+            this.todo.changeTitle(this.todoTitle);
+        }
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="container">
     <Todo :todo="todo" />
+    <Result :todo="todo" />
   </div>
 </template>
 
 <script>
 import Todo from '@/components/Todo';
+import Result from '@/components/Result';
 import { TodoItem } from '@/classes';
 
 export default {
   components: {
     Todo,
+    Result,
   },
   data() {
     return {
